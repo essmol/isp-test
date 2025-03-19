ca665bfefd0d242df4ae5ff473977c9c90e388d0
esmail <esmail@debianl022@gmail.com>
2025-03-19 23:21:59 +0330

    add some changes

--- core/website/urls.py
+++ core/website/urls.py
@@ -3,4 +3,16 @@ from . import views
 
 app_name = "website"
 
-urlpatterns = [path("", views.index_view, name="index")]
+urlpatterns = [
+    
+    path("", views.IndexView.as_view(), name="index"),
+    path("", views.ContactView.as_view(), name="contact"),
+    path("", views.AboutView.as_view(), name="about"),
+    path("", views.PakegeView.as_view(), name="package"),
+    path("", views.BlogView.as_view(), name="blog"),
+    path("", views.FeaturesView.as_view(), name="featuers"),
+               
+               
+               
+               
+               ]
--- core/website/views.py
+++ core/website/views.py
@@ -1,8 +1,38 @@
-from django.shortcuts import render
+from django.shortcuts import render,redirect
+from django.contrib import messages
+from django.urls import reverse_lazy
+from django.views.generic import (
+    TemplateView,
+    ListView,
+    DetailView,
+    FormView,
+    CreateView
+)
 
-# Create your views here.
 
 
-def index_view(request):
-    context = {}
-    return render(request, "website/index.html", context)
+
+
+
+class IndexView(TemplateView):
+    template_name = 'website/index.html'
+
+
+class ContactView(TemplateView):
+    template_name = 'website/contact.html'
+
+
+
+class AboutView(TemplateView):
+    template_name = 'website/about.html'  
+
+class PakegeView(TemplateView):
+    template_name = 'website/package.html' 
+
+
+class BlogView(TemplateView):
+    template_name = 'blog/blog.html' 
+
+
+class FeaturesView(TemplateView):
+    template_name = 'website/featuers.html'             
